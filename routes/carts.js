8f95961220c1e865e479b1cfd2480d9ecc9c3fc5
Ryan Roman <rtroman14@gmail.com>
2020-08-21 22:18:42 -0600

    cart logic

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,26 @@
 const express = require("express");
+const cartsRepo = require("../repositories/carts");
 
 const router = express.Router();
+
+router.post("/cart/products", async (req, res) => {
+    // Episode 434 - 435
+    // Figure out if cart
+    let cart;
+    if (!req.session.cartId) {
+        // We don't have a cart, we need to create one
+        // and store cart id on req.session.cartId property
+        cart = await cartsRepo.create({ items: [] });
+        req.session.cartId = cart.id;
+    } else {
+        // We have a cart! Let's get it from the repository
+        cart = await cartsRepo.getOne(req.session.cartId);
+    }
+
+    // Either incriment existing product quantity
+    // OR add new prodcut to cart array
+    console.log(cart);
+    res.send("Product added to cart");
+});
 
 module.exports = router;
